d5ccf8c8a3f414c248a4219ccd22db9fb6854a6c
Shikhar Srivastava <shikharsrivastava210@gmail.com>
2024-08-25 18:10:43 +0530

    dynamic api and shimmer ui

--- Components/Body.js
+++ Components/Body.js
@@ -1,11 +1,24 @@
 import { useEffect, useState } from "react";
-import { restaurantsDetails } from "../Utils/mockData";
 import { ResturantCard } from "./ResturantCard"
+import { Shimmer } from "./Shimmer";
 
 export const Body = () => {
-    const [listOfResturant, setListOfResturant] = useState(restaurantsDetails);
+    const [listOfResturant, setListOfResturant] = useState([]);
+    const [filteredListOfResturant, setFilteredListOfResturant] = useState([]);
     const [filterFlag, setFilterFlag] = useState(false);
     const [searchText, setSearchText] = useState("");
+
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData = async () => {
+        const response = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await response.json();
+        const data = json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        setListOfResturant(data);
+        setFilteredListOfResturant(data);
+    }
 
     useEffect(()=>{
         filterOnSearch()
@@ -17,33 +30,32 @@ export const Body = () => {
 
     const handleSearch = (e) => {
         setSearchText(e.target.value);
-        // filterOnSearch();
     }
 
     const filterOnSearch = () => {
         if(searchText==="") {
-            setListOfResturant(restaurantsDetails);
+            setFilteredListOfResturant(listOfResturant);
         } else {
-            const filteredList = restaurantsDetails.filter((res)=>res.info.name.includes(searchText));
-            setListOfResturant(filteredList);
+            const filteredList = listOfResturant.filter((res)=>res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+            setFilteredListOfResturant(filteredList);
         }
     }
 
     const filterOnRatingButton = () => {
         if(!filterFlag) {
-            setListOfResturant(restaurantsDetails);
+            setFilteredListOfResturant(listOfResturant);
         } else {
-            const filteredList = restaurantsDetails.filter((res)=> res.info.avgRating>4);
-            setListOfResturant(filteredList);
+            const filteredList = listOfResturant.filter((res)=> res.info.avgRating>4);
+            setFilteredListOfResturant(filteredList);
         }
     }
 
     const handleFilterRatingButton = () => {
         setFilterFlag(!filterFlag);
-        // filterOnRatingButton();
     }
 
     return (
+        filteredListOfResturant.length===0 ? <Shimmer/> : 
         <div className="body">
             <div className="search">
                 <input placeholder="Search" onChange={handleSearch}/>
@@ -54,7 +66,7 @@ export const Body = () => {
                 >{filterFlag ? 'Show all' : 'Filter rating above 4'}</button>
             </div>
             <div className="resturant-container">
-            {listOfResturant.map(({ info }) => {
+            {filteredListOfResturant.map(({ info }) => {
                 return <ResturantCard key={info.id} name ={info.name} cuisines={info.cuisines} avgRating={info.avgRating} costForTwo={info.costForTwo} imgId={info.cloudinaryImageId}/>
             })}
             </div>
---
+++ Components/Shimmer.js
@@ -0,0 +1,29 @@
+
+export const Shimmer = () => {
+    return (
+        <div className="body">
+        <div className="search">
+            <input placeholder="Search"/>
+        </div>
+        <div className="filter">
+            <button>Filter rating above 4</button>
+        </div>
+        <div className="shimmer-container">
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+        </div>
+    </div>
+    )
+}
